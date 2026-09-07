b5ca7bdaa9455d18491a8032765b449091d9f2d4
Iwan Bagus S <stywn93@gmail.com>
2026-09-08 02:55:55 +0700

    update CORS configuration to allow specific origins, headers, and methods

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -106,5 +106,7 @@ class Filters extends BaseFilters
      *
      * @var array<string, array<string, list<string>>>
      */
-    public array $filters = [];
+    public array $filters = [
+        'cors' => ['before' => ['api/*']],
+    ];
 }
