ee851738fc9edc1b48cf840518896acd3d8b73d1
Iwan Bagus S <stywn93@gmail.com>
2026-09-08 14:45:57 +0700

    add getSchedule endpoint and implement schedule retrieval logic

--- app/Models/ScheduleRegistrationModel.php
+++ app/Models/ScheduleRegistrationModel.php
@@ -56,4 +56,44 @@ class ScheduleRegistrationModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+
+
+    public function getByRegistrationId(int $idRegistration): array
+{
+    $query = $this->select([
+        'sr.id_registration',
+        'sm.antigen_name',
+        "DATE_FORMAT(sr.ideal_start_date, '%d %M %Y') AS ideal_start_date",
+        "DATE_FORMAT(sr.ideal_end_date, '%d %M %Y') AS ideal_end_date",
+        "DATE_FORMAT(sr.catchup_start_date, '%d %M %Y') AS catchup_start_date",
+        "DATE_FORMAT(sr.catchup_end_date, '%d %M %Y') AS catchup_end_date",
+        "DATE_FORMAT(sr.last_catchup_start_date, '%d %M %Y') AS last_catchup_start_date",
+        "DATE_FORMAT(sr.last_catchup_end_date, '%d %M %Y') AS last_catchup_end_date",
+        "DATE_FORMAT(sr.reminder_sent_at, '%d %M %Y') AS reminder_sent_at",
+        'w_province.nama AS province_name',
+        'r.mother_name',
+        "DATE_FORMAT(r.dob_baby, '%d %M %Y') AS dob_baby",
+        'r.gender_baby',
+        'w_district.nama AS district_name',
+        'w_subdistrict.nama AS subdistrict_name',
+        'w_village.nama AS village_name',
+        'r.whatsapp',
+        'r.email',
+    ])
+        ->from('schedule_registrations sr', true)
+        ->join('registrations r', 'r.id = sr.id_registration', 'left')
+        ->join('schedule_master sm', 'sm.id = sr.id_schedule', 'left')
+        ->join('wilayah w_province', 'w_province.kode = r.province', 'left')
+        ->join('wilayah w_district', 'w_district.kode = r.district', 'left')
+        ->join('wilayah w_subdistrict', 'w_subdistrict.kode = r.subdistrict', 'left')
+        ->join('wilayah w_village', 'w_village.kode = r.village', 'left')
+        ->where('sr.id_registration', $idRegistration)
+        ->get();
+
+    log_message('debug', 'ScheduleRegistrationModel query: ' . (string) $this->db->getLastQuery());
+
+    return $query->getResultArray();
+}
+
 }
